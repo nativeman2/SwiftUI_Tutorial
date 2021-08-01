fb8fb7a6cef64478c21a7f3b1240e33a116c08f3
Ayodeji Onipe <ayosemail@gmail.com>
2021-08-01 12:58:34 -0400

    Composing Complex Interfaces: Complete 🤩

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -14,6 +14,18 @@ final class ModelData: ObservableObject {
     // so that its subscribers can pick up the change.
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary (
+            grouping: landmarks,
+            by: {$0.category.rawValue}
+        )
+    }
+
 
 }
 
