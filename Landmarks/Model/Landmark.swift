d57459964c05a584997d53b52ceb6b30d6e8f8f7
Ayodeji Onipe <ayosemail@gmail.com>
2021-08-15 16:52:31 -0400

    Interfacing with UIKit: Complete🤪

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -34,6 +34,10 @@ struct Landmark: Hashable, Codable, Identifiable {
     var image: Image {
         Image(imageName)
     }
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+
 
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
